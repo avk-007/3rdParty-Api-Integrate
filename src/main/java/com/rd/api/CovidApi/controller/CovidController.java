c69c209e89cf24415b974af76bc4a850203065de
“[avk-007]” <[abhik7july@gmail.com]>
2023-12-14 21:13:51 +0530

    firstCommit of 3rd party api

---
+++ src/main/java/com/rd/api/CovidApi/controller/CovidController.java
@@ -0,0 +1,22 @@
+package com.rd.api.CovidApi.controller;
+
+
+import com.rd.api.CovidApi.service.CovidService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/covid")
+@RequiredArgsConstructor
+public class CovidController {
+
+    private final CovidService covidService;
+    @GetMapping("/get-all-country-covid-data")
+    public ResponseEntity<?> callRapidEndpointToGetCovidData(){
+        return ResponseEntity.ok(covidService.getAllCountryCovidData());
+
+    }
+}
